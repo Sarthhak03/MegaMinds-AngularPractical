442f754440c95a2e07f17f649f429cf33aa73bff
Sarthhak <Sarthhak_Kachwha@outlook.com>
2023-05-28 17:07:22 +0530

    folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DetailViewComponent } from './components/detail-view/detail-view.component';
+import { SummaryViewComponent } from './components/summary-view/summary-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailViewComponent,
+    SummaryViewComponent
   ],
   imports: [
     BrowserModule,
