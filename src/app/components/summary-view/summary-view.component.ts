22acb0b160607d1a5aa48096ce797a62f5e44951
Sarthhak <Sarthhak_Kachwha@outlook.com>
2023-05-30 13:45:44 +0530

    Summary View & fixed primeng Styles

--- src/app/components/summary-view/summary-view.component.ts
+++ src/app/components/summary-view/summary-view.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { ConstructionData } from 'src/app/interfaces/constructionData.interface';
 import { ProjectDetails } from 'src/app/interfaces/projectDetails.interface';
 import { CommonService } from 'src/app/services/common.service';
+
+interface SimplifiedObject {
+  SamplingTime: string;
+  [key: string]: any;
+}
 
 @Component({
   selector: 'app-summary-view',
@@ -8,7 +14,9 @@ import { CommonService } from 'src/app/services/common.service';
   styleUrls: ['./summary-view.component.scss'],
 })
 export class SummaryViewComponent implements OnInit {
-  public projectDetails: ProjectDetails[] = [];
+
+  public projectDetails: ConstructionData[] = [];
+  public tableData : SimplifiedObject[] = [];
 
   constructor(private service: CommonService) {}
 
@@ -22,6 +30,7 @@ export class SummaryViewComponent implements OnInit {
         const apiResponse = res;
         this.projectDetails = res.Datas;
         console.log(this.projectDetails);
+        this.mappingFunction(this.projectDetails);
       },
       (err) => {
         console.warn(err);
@@ -30,5 +39,24 @@ export class SummaryViewComponent implements OnInit {
         console.log('getAllData api called');
       }
     );
+  }
+
+  //simplifying nested json to a custom simple object
+  mappingFunction(projectDetailsArray: ConstructionData[]) {
+    // const simplifiedArray = []
+    projectDetailsArray.forEach((x) => {
+
+      const simplifiedObject: SimplifiedObject = {
+        SamplingTime: x.SamplingTime,
+      };
+
+      x.Properties.forEach((x) => {
+        simplifiedObject[x.Label] = x.Value
+      })
+
+      this.tableData.push(simplifiedObject);
+    });
+
+    console.log('simplifiedArray =>' , this.tableData);
   }
 }
